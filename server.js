a122c9d9f48389b7905e5d2449623b8765267cb6
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-07 23:52:56 +0200

    new file structure

--- server.js
+++ server.js
@@ -58,6 +58,25 @@ app.post('/api/v1/jobs', (req, res) => {
   res.status(201).json({ job });
 });
 
+// edit job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' });
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id == id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
