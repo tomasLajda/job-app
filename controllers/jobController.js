279888a7adb126f6ec099d8d46dc564eb7b866f8
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-15 23:15:32 +0200

    create job converted to work with db

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid';
 
+import Job from '../models/jobMode.js';
+
 let jobs = [
   { id: nanoid(), company: 'apple', position: 'front-end' },
   { id: nanoid(), company: 'google', position: 'back-end' },
@@ -10,14 +12,8 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
+  const job = await Job.create(req.body);
 
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
   res.status(201).json({ job });
 };
 
